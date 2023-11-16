5f7bba3310bbe70b10156b2f6d2e576500999f5c
pavel <evrik@mail.ru>
2023-11-16 21:44:48 +0300

    backend of lection with project specific naming

---
+++ src/main/java/com/example/unik/CargoService.java
@@ -0,0 +1,32 @@
+package com.example.unik;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CargoService {
+  @Autowired
+  private CargoRepository repo;
+
+  public List<Cargo> listAll(String keyword){
+      if (keyword != null){
+          return repo.search(keyword);
+      }
+      return repo.findAll();
+  }
+
+  public void save(Cargo cargo) {
+    repo.save(cargo); 
+  }
+
+  public Cargo get(Long id) {
+    return repo.findById(id).get(); 
+  }
+
+  public void delete(Long id) {
+    repo.deleteById(id);
+    
+  }
+}
--- src/main/java/com/example/unik/UnikApplication.java
+++ src/main/java/com/example/unik/UnikApplication.java
@@ -2,9 +2,10 @@ package com.example.unik;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class UnikApplication {
+public class UnikApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(UnikApplication.class, args);
