3ae43265918e11e652df1def1c79b0cc6ff842ff
NikitaUshanov <nikita.ushanov.y@gmail.com>
2024-01-07 23:34:00 +0300

    ROLES | add models

---
+++ src/main/java/com/example/unik/Posts.java
@@ -0,0 +1,39 @@
+package com.example.unik;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Entity
+public class Posts {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long Id;
+
+    @Setter
+    @Column(name="topic")
+    private String Topic;
+
+    @Setter
+    @Column(name="post_text")
+    private String PostText;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(name = "user_posts_reader",
+            joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "post_id", referencedColumnName = "id"))
+    private Set<Posts> readers = new HashSet<>();
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(name = "user_posts_owner",
+            joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "post_id", referencedColumnName = "id"))
+    private Set<Posts> owners = new HashSet<>();
+
+    protected Posts(){
+    }
+}
